41351b63f1a3ce9c56bf9cdae36353c7b57fbb90
Federico Valenzuela <fvalenzuela061@gmail.com>
2020-08-25 14:26:41 -0300

    script editado

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
+// //De esta forma se inicializa primero el doc de html y los scripts (forma vieja)
+// document.addEventListener('DomContentLoaded', function (event) {
+    
+// });
 
-});
+// //De esta forma se inicializa primero el doc de html y los scripts (forma nueva)
+// $(document).ready(function(){
 
-document.addEventListener('DomContentLoaded', function (event) {
+// });
+
+//De esta forma se inicializa primero el doc de html y los scripts (forma nueva corta)
+$(function() {
+    'use strict';
     
 });
