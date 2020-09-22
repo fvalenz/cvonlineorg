20e98a762cfea24d8e704f037303bdce916417ae
Federico Valenzuela <fvalenzuela061@gmail.com>
2020-09-22 17:15:40 -0300

    Cargo Preloader

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,14 @@
 
 // });
 
+// $(document).ready(function () {
+//     $('#preloader').delay(2000);
+//     $('#preloader').fadeOut(1300);
+// });
+
 //De esta forma se inicializa primero el doc de html y los scripts (forma nueva corta)
 $(function() {
     'use strict';
-    
+    $('#preloader').delay(800);
+    $('#preloader').fadeOut(1000);
 });
